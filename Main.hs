c3d5723cc707da2a8296b2a62d2b90a7d80d6bd6
John Orford <john.orford@gmail.com>
2024-03-08 22:18:20 +0100

    update

--- Main.hs
+++ Main.hs
@@ -1,16 +1,19 @@
 import Control.Exception (SomeException, catch)
 import Control.Monad (join)
-import NonZero (NonZero (..), getNonZero, mkNonZero)
+import NonZero (NonZero (..), mkNonZero)
 
--- import NumberType
+--
+--- 1) Imperative style?
 
 -- Attempt division and catch any exceptions, returning Nothing in case of an exception
-safeDivEx :: Int -> Int -> IO (Maybe Int)
-safeDivEx _ 0 = return Nothing -- Immediate return of Nothing if dividing by 0
-safeDivEx numerator denominator =
+
+safeDivEx :: Int -> Int -> Int -> IO (Maybe Int)
+safeDivEx _ _ 0 = return Nothing -- Immediate return of Nothing if dividing by 0
+safeDivEx _ 0 _ = return Nothing -- Immediate return of Nothing if dividing by 0
+safeDivEx a b c =
   catch
     ( do
-        let result = numerator `div` denominator
+        let result = a `div` b `div` c
         return $ Just result
     )
     handler
@@ -18,12 +21,8 @@ safeDivEx numerator denominator =
     handler :: SomeException -> IO (Maybe Int)
     handler _ = return Nothing
 
-safeDiv :: (Eq a, Fractional a) => a -> a -> Maybe a
-safeDiv _ 0 = Nothing
-safeDiv x y = Just (x / y)
-
-nonZeroDiv :: (Fractional a, Eq a) => a -> NonZero a -> a
-nonZeroDiv numerator (NonZero denominator) = numerator / denominator
+--
+--- 2) Maybes in, Maybes out
 
 maybeDiv :: (Eq a, Fractional a) => Maybe a -> Maybe a -> Maybe a
 maybeDiv (Just n) (Just 0) = Nothing
@@ -31,45 +30,64 @@ maybeDiv (Just n) (Just d) = Just (n / d)
 maybeDiv Nothing _ = Nothing
 maybeDiv _ Nothing = Nothing
 
--- not idomatic, but v nice
+-- not idomatic, but v nice(?)
 maybeDivEx :: Maybe Double
 maybeDivEx = Just 16 `maybeDiv` Just 2 `maybeDiv` Just 0
 
+--
+--- 3) Idiomatic style
+
+safeDiv :: (Eq a, Fractional a) => a -> a -> Maybe a
+safeDiv _ 0 = Nothing
+safeDiv x y = Just (x / y)
+
 -- likeable apart from the join
-fmapEx :: Maybe Double
-fmapEx = join $ fmap (16 `safeDiv`) (2 `safeDiv` 1)
+
+fmapEx :: (Eq a, Fractional a) => a -> a -> a -> Maybe a
+fmapEx a b c = join $ fmap (a `safeDiv`) (b `safeDiv` c)
 
 -- Clunky
-doEx :: Maybe Double
-doEx = do
-  a <- 16 `safeDiv` 2
-  a `safeDiv` 1
+
+doEx :: (Eq b, Fractional b) => b -> b -> b -> Maybe b
+doEx a b c = do
+  a' <- a `safeDiv` b
+  a' `safeDiv` c
 
 -- most idiomatic?
-bindEx :: Maybe Double
-bindEx = (16 `safeDiv`) =<< (2 `safeDiv` 1)
+bindEx :: (Eq b, Fractional b) => b -> b -> b -> Maybe b
+bindEx a b c = (a `safeDiv`) =<< (b `safeDiv` c)
+
+--
+--- 4) Utilising NaN funkiness
 
 -- NaN is part of Float type
-simpleEx :: Double -> Double -> Double -> Maybe Double
+
+simpleEx :: (RealFloat a) => a -> a -> a -> Maybe a
 simpleEx a b c =
   let result = a / b / c
    in if isNaN result then Nothing else Just result
 
+--
+--- 5) Naive Example
+
 -- Can cause exception. Bad idea
-simpleDivEx :: (Integral a) => a -> a
-simpleDivEx a = a `div` a
+simpleDivEx :: (Integral a) => a -> a -> a -> a
+simpleDivEx a b c = a `div` b `div` c
 
--- Does this do much better than a Maybe?
-nonZeroEx :: (Eq b, Fractional b) => b -> b -> Maybe b
-nonZeroEx a b = do
-  d <- mkNonZero b
-  return $ a `nonZeroDiv` d
+--
+--- 6) Smart Constructor
 
--- data Number a = Zero | NonZero' a deriving (Show)
+nonZeroDiv :: (Fractional a, Eq a) => a -> NonZero a -> a
+nonZeroDiv numerator (NonZero denominator) = numerator / denominator
 
--- divNumber :: (Fractional a) => Number a -> NonZero a -> Number a
--- divNumber Zero _ = Zero -- Dividing zero by any non-zero number is zero
--- divNumber (NonZero' n) (NonZero d) = NonZero' (n / d) -- Normal division case
+-- not convinced
+nonZeroEx :: (Eq b, Fractional b) => b -> b -> b -> Maybe b
+nonZeroEx a b c = do
+  d1 <- mkNonZero b
+  d2 <- mkNonZero c
+  return $ a `nonZeroDiv` d1 `nonZeroDiv` d2
+
+---
 
 main :: IO ()
 main = do
@@ -77,20 +95,19 @@ main = do
 
 test :: IO ()
 test = do
-  putStrLn $ "fmapEx: " <> show fmapEx
+  sdx <- safeDivEx 16 2 1
+  putStrLn $ "safeDivEx: " <> show sdx
 
-  putStrLn $ "bindEx: " <> show bindEx
+  putStrLn $ "fmapEx: " <> show (fmapEx 16 2 1)
 
-  putStrLn $ "doEx: " <> show doEx
+  putStrLn $ "bindEx: " <> show (bindEx 16 2 1)
+
+  putStrLn $ "doEx: " <> show (doEx 16 2 1)
 
   putStrLn $ "simpleEx: " <> show (simpleEx 16 2 1)
 
-  -- result1 <- safeDivEx 16 2
-  -- result2 <- safeDivEx result1 1
-  -- putStrLn $ "safeDivEx: " <> show result2
-
   putStrLn $ "maybeDivEx: " <> show maybeDivEx
 
-  putStrLn $ "nonZeroEx: " <> show (nonZeroEx 0 1)
+  putStrLn $ "nonZeroEx: " <> show (nonZeroEx 16 2 1)
 
-  putStrLn $ "simpleDivEx: " <> show (simpleDivEx 0)
+  putStrLn $ "simpleDivEx: " <> show (simpleDivEx 16 2 0)
